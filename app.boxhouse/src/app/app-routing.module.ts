4d0db88e95988aaad7005412efd4487864695268
Evan Rougetet <evanom21@outlook.com>
2022-10-26 10:48:38 +0200

    gwen casse tout

--- app.boxhouse/src/app/app-routing.module.ts
+++ app.boxhouse/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { SigninComponent } from './signin/signin.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent, },
-  { path: 'signin', component: SigninComponent },
 ];
 
 @NgModule({
