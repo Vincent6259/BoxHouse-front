d05afbb7bc9542d97932d7231877529881b4b8cf
gwenauger <73801080+gwenauger@users.noreply.github.com>
2022-10-26 11:04:31 +0200

    0.0.1

--- app.boxhouse/src/app/app.module.ts
+++ app.boxhouse/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SigninComponent } from './signin/signin.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
