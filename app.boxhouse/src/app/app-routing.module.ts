2d410bc293e995ea23647c3adedd6383fd61ae63
gwenauger <73801080+gwenauger@users.noreply.github.com>
2022-10-26 10:21:02 +0200

    send V1

---
+++ app.boxhouse/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent, },
+  { path: 'signin', component: SigninComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
