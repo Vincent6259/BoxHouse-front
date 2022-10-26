ab1d9f23563fa77dd5ab1c380e0a26baff410f58
Evan Rougetet <evanom21@outlook.com>
2022-10-26 10:27:53 +0200

    ntm gwen

--- app.boxhouse/src/test.ts
+++ app.boxhouse/src/test.ts
@@ -20,7 +20,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting(),
 );
 
-// Then we find all the tests.
+// Then we find al the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().forEach(context);
