b3326c6e36066ca63731cdc1d20f3315111a08b9
Evan Rougetet <evanom21@outlook.com>
2022-11-25 11:40:54 +0100

    Account à jour

--- app.boxhouse/src/app/account/account.component.ts
+++ app.boxhouse/src/app/account/account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Auth } from '../core/Auth';
 @Component({
   selector: 'app-account',
   templateUrl: './template.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
   private session
+  private auth = new Auth()
 
   constructor() {
 
@@ -19,12 +20,18 @@ export class AccountComponent implements OnInit {
   async load(){
     await this.loadMail()
     await this.loadFirstname()
+    await this.loadLastname()
   }
   async logout(){
     this.session = null
   }
 
-  async loadMail(){}
+  async loadMail(){
+    let self = this
+    let el   = document.getElementById('PageAccount_mail')
+    console.log(this.session.core.auth.mail)
+    el.innerHTML = this.session.core.auth.mail
+  }
 
   async loadFirstname(){
     let self = this
@@ -32,5 +39,27 @@ export class AccountComponent implements OnInit {
     console.log(this.session.core.auth.firstname)
     el.innerHTML = this.session.core.auth.firstname
   }
+  async loadLastname(){
+    let self = this
+    let el   = document.getElementById('PageAccount_lastname')
+    console.log(this.session.core.auth.lastname)
+    el.innerHTML = this.session.core.auth.lastname
+  }
 
+  async bindEvents(){
+    let self = this
+    let logout_button = document.getElementById('PageLogout_submit') as any
+
+    logout_button.addEventListener('click', async function(e) {
+      await self.doLogout()
+      return 0
+    })
+
+  }
+
+    async doLogout(){
+      this.auth.disconnect()
+    }
+
+  
 }
