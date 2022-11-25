44888882a4260f48a9ea8eb10c472a1696907af4
gwenauger <73801080+gwenauger@users.noreply.github.com>
2022-11-25 11:49:51 +0100

    0.0.23

--- app.boxhouse/src/app/menu/menu.component.ts
+++ app.boxhouse/src/app/menu/menu.component.ts
@@ -19,12 +19,12 @@ export class MenuComponent implements OnInit {
   async bindEvents(){
     let self = this
 
-    let button_account = document.getElementsByTagName('button')[0]
-    let button_home    = document.getElementsByTagName('button')[1]
-    let button_box     = document.getElementsByTagName('button')[2]
-    let button_object  = document.getElementsByTagName('button')[3]
-    let button_qrcode  = document.getElementsByTagName('button')[4]
-    let button_scan    = document.getElementsByTagName('button')[5]
+    let button_account = document.getElementsByTagName('menu')[0].getElementsByTagName('button')[0]
+    let button_home    = document.getElementsByTagName('menu')[0].getElementsByTagName('button')[1]
+    let button_box     = document.getElementsByTagName('menu')[0].getElementsByTagName('button')[2]
+    let button_object  = document.getElementsByTagName('menu')[0].getElementsByTagName('button')[3]
+    let button_qrcode  = document.getElementsByTagName('menu')[0].getElementsByTagName('button')[4]
+    let button_scan    = document.getElementsByTagName('menu')[0].getElementsByTagName('button')[5]
 
     button_account.addEventListener('click', function(e){
       window.location.href = settings.url.frontend+'/account'
@@ -47,6 +47,7 @@ export class MenuComponent implements OnInit {
       alert("Pas encore disponible")
       //window.location.href = settings.url.frontend+'/account'
     })
+
   }
   async load(){
 
@@ -54,23 +55,30 @@ export class MenuComponent implements OnInit {
     await this.loadSegment()
   }
   async loadColors(){
-
-    let buttons      = document.getElementsByTagName('button')
     let filter_color = await this.toolbox.hexToFilter("#FFFFFF")
+    let buttons      = document.getElementsByTagName('menu')[0].getElementsByTagName('button')
     for(let i = 0; i < 4; i++ ){
       buttons[i].getElementsByTagName('img')[0].style.filter = filter_color.split(':')[1].slice(0, -1)
     }
+
   }
 
   async loadSegment(){
-    if(number == 0){
-      document.getElementsByClassName('bar')
-    }
-    if(number == 2){}
-    if(number == 3){}
-    if(number == 4){}
-    if(number == 5){}
-
+    if(window.location.href.includes('account'))
+      this.loadBar(0)
+    if(window.location.href.includes('home'))
+      this.loadBar(1)
+    if(window.location.href.includes('box'))
+      this.loadBar(2)
+    if(window.location.href.includes('object'))
+      this.loadBar(3)
+    if(window.location.href.includes('qrcode'))
+      this.loadBar(4)
+    if(window.location.href.includes('scan'))
+      this.loadBar(5)
+  }
+  async loadBar(number){
+    document.getElementsByClassName('bar')
   }
 
 }
